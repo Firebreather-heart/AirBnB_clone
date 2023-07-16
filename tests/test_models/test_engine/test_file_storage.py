9017c70a8baa27600caed1df86f7f6bee10df673
IkeSeny0 <ikesenyo.im@gmail.com>
2023-07-16 16:51:34 +0000

    tests are working

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Test for storage
+"""
+from datetime import datetime
+import unittest
+from time import sleep
+import json
+from AirBnB_clone.models.engine.file_storage import FileStorage
+
+
+class test_fileStorage(unittest.TestCase):
+    """Test FileStorage Class"""
+    def test_instances(self):
+        """chequeamos instantation"""
+        obj = FileStorage()
+        self.assertIsInstance(obj, FileStorage)
+
+    def test_docs(self):
+        """Test docstrings"""
+        self.assertIsNotNone(FileStorage.all)
+        self.assertIsNotNone(FileStorage.new)
+        self.assertIsNotNone(FileStorage.save)
+        self.assertIsNotNone(FileStorage.reload)
+
+    if __name__ == '__main__':
+        unittest.main()
+
